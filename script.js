49d2cb35f8d0033a4642f3df1cbc74491c8ca395
Matias Catalan <macatalanse@falabella.cl>
2024-03-09 23:25:32 -0300

    adding changes in carrousel and other fixes

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     $('#carouselExampleIndicators').carousel();
 });
 
+$('#carouselExampleControls').on('slid.bs.carousel', function () {
+    if ($('.carousel-inner .carousel-item:last').hasClass('active')) {
+        $(this).carousel('pause');
+        setTimeout(function () {
+            $('#carouselExampleControls').carousel(0);
+        }, 1000); // Cambia el valor de este retraso según tus preferencias
+    } else if ($('.carousel-inner .carousel-item:first').hasClass('active')) {
+        setTimeout(function () {
+            $('#carouselExampleControls').carousel('cycle');
+        }, 1000); // Cambia el valor de este retraso según tus preferencias
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const zoomEffects = document.querySelectorAll('.zoom-effect');
     
