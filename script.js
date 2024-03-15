b56c5844a0342a34e2ccc151bc72f24a636eaeba
Matias Catalan <macatalanse@falabella.cl>
2024-03-15 18:08:24 -0300

    cambios fotos en modal

--- script.js
+++ script.js
@@ -63,3 +63,20 @@ document.addEventListener("DOMContentLoaded", function() {
       window.open("https://api.whatsapp.com/send?phone=56999750521&text=%F0%9F%91%8B%20Hola,%20me%20gustaría%20realizar%20un%20pedidos", "_blank");
     });
   });
+
+// JavaScript para abrir y cerrar el modal de fotos
+
+function openModal(imgSrc) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("imgModal");
+    modal.style.display = "flex"; // Cambia el display a flex
+    modalImg.src = imgSrc;
+    modal.scrollTop = 0; // Asegura que el modal esté en la parte superior
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+
